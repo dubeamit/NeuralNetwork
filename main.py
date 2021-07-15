eb9c822fd42ef91c24d4c0a7541e0233db227337
dube.amit <dube.amit15@gmail.com>
2021-07-15 23:35:55 +0530

    added layer class

--- main.py
+++ main.py
@@ -6,23 +6,6 @@ def preprocess(img):
     img = img / 255
     return img.flatten()
 
-def sigmoid(x):
-    return 1 /(1 + np.exp(-x))
-
-def sigmoid_d(x):
-    return sigmoid(x) * (1 - sigmoid(x))
-
-def feedforward(input, weights):
-    convolution = np.dot(input, weights)
-    output = sigmoid(convolution)
-    return output
-
-def backpropogation(output, label, input):
-    error = output - label
-    adjustment = error * sigmoid_d(output)
-    weights_adjustment = np.dot(adjustment, input)
-    return weights_adjustment
-
 
 # 1. read image
 horizontal_img = cv2.imread('images/horizontal.png', 0)
@@ -34,16 +17,39 @@ vertical_img = preprocess(cv2.resize(vertical_img, (3,3), interpolation=0))
 # print(horizontal_img)
 
 dataset = np.array([vertical_img, horizontal_img])
-labels = np.array([0, 1])
+labels = np.array([[0, 1]]).T
 
-# 2. weights
-weights = np.array([0.5]*9)
-# print(weights.shape)
 
+
+class Layers:
+    def __init__(self, input_size, output_size):
+        print('Initialzing layers')
+        self.weights = np.random.rand(input_size, output_size) # output between 0-1
+
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def sigmoid_d(self, x):
+        return self.sigmoid(x) * (1 - self.sigmoid(x))
+
+    def feedword(self, input):
+        convolution = np.dot(input, self.weights)
+        output = self.sigmoid(convolution)
+        return output
+
+    def backpropogation(self, output, labels, input):
+        error = output - labels
+        adjustment = error * self.sigmoid_d(output)
+        weights_adjustment = np.dot(input.T, adjustment)
+        self.weights -= weights_adjustment
+
+        
+# create the layers
+output_layer = Layers(9, 1)
 epochs = 100
-for i in range(epochs):
-    output = feedforward(dataset, weights)
-    weights -= backpropogation(output, labels, dataset)
-    # print(weights)
-    
-print(output)
+for epoch in range(epochs):
+    output = output_layer.feedword(dataset)
+    output_layer.backpropogation(output, labels, dataset)
+
+print('final weights', output_layer.weights)
+print('ouput', output)
